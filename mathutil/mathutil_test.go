8ab88fee355fc0662ca2472a4aa3c91bfa8deabc
yigidigulaglati <yigidigulaglati@gmail.com>
2025-11-11 22:08:42 +0300

    added a test suite

--- mathutil/mathutil_test.go
+++ mathutil/mathutil_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestAverage_Basic(t *testing.T){
 	got, err := Average([]float64{1.0, 2.0, 3.0});
-
 
 	require.NoError(t, err);
 	require.Equal(t, 2.0, got);
@@ -76,9 +76,38 @@ func TestAverage_TableDriven(t *testing.T){
 			}
 		})
 	}
-
 }
 
 
+type AverageTestSuite struct{
+	suite.Suite
+}
+
+func (s *AverageTestSuite) TestBasic(){
+
+	got, err := Average([]float64{1.0, 2.0, 3.0});
+
+	require.NoError(s.T(), err);
+	require.Equal(s.T(), 2.0, got);
+}
+
+func (s *AverageTestSuite) TestEmpty(){
+	got, err := Average([]float64{});
+
+	require.Error(s.T(), err);
+	require.Equal(s.T(), 0.0, got);
+	require.EqualError(s.T(), err, "cannot average empty slice");
+}
+
+func (s *AverageTestSuite) TestNegative(){
+	got, err := Average([]float64{-1.0, -2.0, -3.0});
+
+	require.NoError(s.T(), err);
+	require.Equal(s.T(), -2.0, got);
+}
+
+func TestAverageTestSuite(t *testing.T){
+	suite.Run(t, new(AverageTestSuite));
+}
 
 
