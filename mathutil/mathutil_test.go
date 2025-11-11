55f5968b2a76b2884b7e36b577b9ca9b2e7a468a
yigidigulaglati <yigidigulaglati@gmail.com>
2025-11-11 21:42:42 +0300

    added table driven unit test

--- mathutil/mathutil_test.go
+++ mathutil/mathutil_test.go
@@ -29,6 +29,56 @@ func TestAverage_NegativeNumbers(t *testing.T){
 	require.Equal(t, -2.0, got);
 }
 
+func TestAverage_TableDriven(t *testing.T){
+
+
+	tests := []struct{
+		name  string
+		input []float64
+		want float64
+		wantErr bool
+		errMsg string
+	}{
+		{
+			name: `basic average`,
+			input: []float64{1,2,3},
+			want: 2.0,
+			wantErr: false,
+			errMsg: ``,
+		},
+		{
+			name: `empty slice`,
+			input: []float64{},
+			want: 0.0,
+			wantErr: true,
+			errMsg: "cannot average empty slice",
+		},{
+			name: `negative numbers`,
+			input: []float64{-1,-2,-3},
+			want: -2.0,
+			wantErr: false,
+			errMsg: ``,
+		},
+	}
+
+	for _, tt := range tests{
+
+		t.Run(tt.name, func(t *testing.T){
+			got, err := Average(tt.input);
+
+			if tt.wantErr{
+				require.Error(t, err);
+				require.EqualError(t, err, tt.errMsg);
+				require.Equal(t, tt.want, got)
+			}else{
+				require.NoError(t, err);
+				require.Equal(t, tt.want, got);
+			}
+		})
+	}
+
+}
+
 
 
 
